61ec740fe94bdaf5c9d54ca9223c1346d7da350d
干物檬 <5470395+ganwumg@user.noreply.gitee.com>
2025-03-22 22:40:31 +0800

    Optimise the interaction speed when Tap Event is set to null

--- lib/src/components/day_view_components.dart
+++ lib/src/components/day_view_components.dart
@@ -204,9 +204,9 @@ class FullDayEventView<T> extends StatelessWidget {
         padding: padding ?? EdgeInsets.zero,
         shrinkWrap: true,
         itemBuilder: (context, index) => InkWell(
-          onLongPress: () => onEventLongPress?.call(events, date),
-          onTap: () => onEventTap?.call(events, date),
-          onDoubleTap: () => onEventDoubleTap?.call(events, date),
+          onLongPress: onEventLongPress == null?null:() => onEventLongPress?.call(events, date),
+          onTap: onEventTap == null?null:() => onEventTap?.call(events, date),
+          onDoubleTap: onEventDoubleTap == null?null:() => onEventDoubleTap?.call(events, date),
           child: itemView?.call(events[index]) ??
               Container(
                 margin: const EdgeInsets.all(5.0),
